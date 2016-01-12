5a24b0a9e8fe3566b346f7999218be2002faa4ef
vincent <vincent@gmail.com>
2016-01-12 19:00:26 +0800

    second commit

---
+++ obj/local/armeabi/objs/UpdateSystem/UpdateSystem.o.d
@@ -0,0 +1,137 @@
+/cygdrive/e/vincent1232/UpdateTopSystem/obj/local/armeabi/objs/UpdateSystem/UpdateSystem.o: \
+ /cygdrive/e/vincent1232/UpdateTopSystem/jni/UpdateSystem.c \
+ /cygdrive/e/vincent1232/UpdateTopSystem/jni/iconv.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/errno.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/android/api-level.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/errno.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/errno.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm-generic/errno.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm-generic/errno-base.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/wchar.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/limits.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/limits.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/limits.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/machine/internal_types.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/machine/limits.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/syslimits.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/page.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/stdio.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/types.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/stdint.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/_types.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/machine/_types.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/posix_types.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/stddef.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/compiler.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/posix_types.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/types.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/types.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/machine/kernel.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/time.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/time.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/time.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/siginfo.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm-generic/siginfo.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/malloc.h \
+ /cygdrive/e/vincent1232/UpdateTopSystem/jni/publicIO.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/jni.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/android/log.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/string.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/stdlib.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/alloca.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/strings.h \
+ /cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/memory.h
+
+/cygdrive/e/vincent1232/UpdateTopSystem/jni/iconv.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/errno.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/android/api-level.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/errno.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/errno.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm-generic/errno.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/wchar.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/limits.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/limits.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/limits.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/machine/internal_types.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/machine/limits.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/syslimits.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/page.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/stdio.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/types.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/stdint.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/_types.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/machine/_types.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/posix_types.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/stddef.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/compiler.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/posix_types.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/types.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/types.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/machine/kernel.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/time.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/time.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/time.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/siginfo.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/malloc.h:
+
+/cygdrive/e/vincent1232/UpdateTopSystem/jni/publicIO.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/jni.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/android/log.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/string.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/stdlib.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/alloca.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/strings.h:
+
+/cygdrive/e/vincent/ndk/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/memory.h:
